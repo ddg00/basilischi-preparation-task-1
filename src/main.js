c43b4d507e61a35e93cf10dd8a31f2149437a477
AryaUP <arya.ddg00@gmail.com>
2017-02-21 11:00:51 +0800

    separate task1

--- src/main.js
+++ src/main.js
@@ -4,23 +4,14 @@ import {
   Image,
   View
 } from 'react-native';
-import styles from './style';
-import peserta from './peserta';
+import Task1 from './task1/index';
 
 // line 16 <Image source={require('./img/'+peserta.image)} /> not working
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Image source={require('./img/profile_image_arya.png')} />
-        <Text style={styles.batch}>
-          {peserta.batch}
-        </Text>
-        <Text style={styles.name}>
-          {peserta.name}
-        </Text>
-      </View>
+      <Task1 />
     );
   }
 }
---
+++ src/task1/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  Image,
+  View
+} from 'react-native';
+import styles from './style';
+import peserta from './peserta';
+
+// line 16 <Image source={require('./img/'+peserta.image)} /> not working
+
+class task1 extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('../img/profile_image_arya.png')} />
+        <Text style={styles.batch}>
+          {peserta.batch}
+        </Text>
+        <Text style={styles.name}>
+          {peserta.name}
+        </Text>
+      </View>
+    );
+  }
+}
+
+
+export default task1;
